03eef06392221ad6156ac41cc331bfe2f07a1c0b
janis-jenny <pcardenasvm@gmail.com>
2020-11-06 23:54:24 -0500

    add more join

--- 5. Join.sql
+++ 5. Join.sql
@@ -1,8 +1,8 @@
--- 1. Modify it to show the matchid and player name for all goals scored by Germany
+-- 1. Modify it to show the match id and player name for all goals scored by Germany
 SELECT matchid, player FROM goal WHERE teamid = 'GER';
 
 -- 2. Show id, stadium, team1, team2 for just game 1012
-SELECT id,stadium,team1,team2 FROM game;
+SELECT id,stadium,team1,team2 FROM game WHERE id = 1012;
 
 -- 3. Modify it to show the player, teamid, stadium and mdate for every German goal
 SELECT player, teamid, stadium, mdate FROM game JOIN goal ON (id=matchid)
